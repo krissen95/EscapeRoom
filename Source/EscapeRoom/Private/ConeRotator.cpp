78411798e6b0f830588e0f466ae1fb7ecbdece69
krissen95 <krissen@hotmail.no>
2019-02-12 19:15:53 +0100

    Player now working
    Added Player and gamemode blueprint
    Scaled up floor

---
+++ Source/EscapeRoom/Private/ConeRotator.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ConeRotator.h"
+
+
+// Sets default values for this component's properties
+UConeRotator::UConeRotator()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UConeRotator::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Find the owning Actor
+	AActor* Owner = GetOwner();
+	//Create a rotator YZX
+	FRotator NewRotation = FRotator(30.0f, 60.0f, 90.0f);
+	//Set door rotation
+	Owner->SetActorRotation(NewRotation);
+	// ...
+	
+}
+
+
+// Called every frame
+void UConeRotator::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
